d9b4aa7023775a40ec66e54043f78e4529b8fbed
mario-bustamante <bustamante41@yahoo.com>
2026-04-20 23:34:21 -0400

    Integrate Persistence in Flutter

---
+++ lib/country_list.dart
@@ -0,0 +1,17 @@
+Future<List<String>> fetchCountries() async {
+  List<String> countries = [
+    'United States',
+    'Canada',
+    'United Kingdom',
+    'Australia',
+    'India',
+    'Germany',
+    'France',
+    'Japan',
+    'China',
+    'Brazil',
+    'South Africa'
+  ];
+
+  return countries;
+}
